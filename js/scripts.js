221f2db06937a7bd0adde018d656a7c8926a459f
Moi <moisdan777@gmail.com>
2024-04-26 09:31:23 -0500

    probando js en el form

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+// *ejercicio de enviar los datos a un objeto*
+
+// const datos = {
+//     name: ``,
+//     email: ``,
+//     mensaje: ``
+// }
+
+const LabelNombre = document.querySelector(`#name`)
+const LabelEmail = document.querySelector(`#email`)
+const LabelMensaje = document.querySelector(`#mensaje`)
+
+// LabelNombre.addEventListener(`input`, enviarTexto)
+// LabelEmail.addEventListener(`input`, enviarTexto)
+// LabelMensaje.addEventListener(`input`, enviarTexto)
+
+
+
+// function enviarTexto(e){
+//     datos[e.target.id] = e.target.value
+//      console.log(datos)
+// }
+
+
+
+// *Validacion del formulario* 
+
+
+// LabelNombre.addEventListener(`change`, enviarTexto)
+// LabelEmail.addEventListener(`change`, enviarTexto)
+// LabelMensaje.addEventListener(`change`, enviarTexto)
+
+
+// LabelNombre.addEventListener(`change`,())
+
+
+
